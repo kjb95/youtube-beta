834f2a3c515e37c7b89833cf1870412bf28a7776
kjb95 <jinbkim42@gmail.com>
2022-07-14 18:58:42 +0900

    Feat: Spring Initializr

---
+++ backend/src/main/java/com/kjb95/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.kjb95.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
